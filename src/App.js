fa680db33c42e6596e4034a1f9e7c8c70f7d2061
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-08-02 23:46:58 +0530

    feat: add required HTML, CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <>
+      <div className="app">
+        <div className="app-main container">
+          <span>
+            <div className="app-time">17:56</div>
+            <div className="app-greeting">Good Evening, Neeraj</div>
+          </span>
+          <span>
+            <span className="app-text">What is your main focus for today?</span>
+            {/* <input className="app-input" /> */}
+            <span className="app-focus-text">your focus text comes here</span>
+          </span>
+        </div>
+        <div className="app-quote container">
+          "A garden is always a series of losses set against a few triumphs,
+          like life itself."
+        </div>
+        <div className="app-weather container">Weather</div>
+        <div className="app-todo">
+          {/* <div className="container"></div> */}
+          <span>Todo</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
