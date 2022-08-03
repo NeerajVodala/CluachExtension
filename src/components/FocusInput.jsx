f2b464a314c47209a1149133723fcd15ffbccfaf
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-08-04 00:22:02 +0530

    dynamic data feeding using useState

--- src/components/FocusInput.jsx
+++ src/components/FocusInput.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export const FocusInput = () => {
+  const [username, setUsername] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("yes");
+  };
+
   return (
-    <>
+    <form onSubmit={(e) => submitHandler(e)}>
       <span className="app-text">What is your main focus for today?</span>
-      <input className="app-input" />
-    </>
+      <input
+        className="app-input"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
+    </form>
   );
 };
