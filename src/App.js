b6c0c1b5de9e586636ffa6f8e7121cb11269fad8
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-08-03 20:05:40 +0530

    refactor: remove code, import them as components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import "./App.css";
+import { Quote, Weather } from "./components";
+import { Main } from "./pages/Main";
 
 function App() {
   return (
     <>
       <div className="app">
-        <div className="app-main container">
-          <span>
-            <div className="app-time">17:56</div>
-            <div className="app-greeting">Good Evening, Neeraj</div>
-          </span>
-          <span>
-            <span className="app-text">What is your main focus for today?</span>
-            {/* <input className="app-input" /> */}
-            <span className="app-focus-text">your focus text comes here</span>
-          </span>
-        </div>
-        <div className="app-quote container">
-          "A garden is always a series of losses set against a few triumphs,
-          like life itself."
-        </div>
-        <div className="app-weather container">Weather</div>
-        <div className="app-todo">
-          {/* <div className="container"></div> */}
-          <span>Todo</span>
-        </div>
+        <Main />
+        <Quote />
+        <Weather />
       </div>
     </>
   );
